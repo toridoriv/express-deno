7f4ab29b6def231a4d0bca50bf7201463e888999
toridoriv <vrodriguezfe@icloud.com>
2023-10-26 22:16:48 -0300

    :label: Make changes for type improvement

--- mod.ts
+++ mod.ts
@@ -1,4 +1,7 @@
-// @deno-types="./src/express.d.ts";
-import { default as express } from "./src/main.js";
+import type { e } from "./src/express.d.ts";
+import express from "./src/express.js";
 
-export default express;
+/**
+ * Creates an Express application. The express() function is a top-level function exported by the express module.
+ */
+export default express as typeof e;
--- src/express.d.ts
+++ src/express.d.ts
@@ -15,9 +15,9 @@ import type * as serveStatic from "npm:@types/serve-static";
 /**
  * Creates an Express application. The express() function is a top-level function exported by the express module.
  */
-declare function e(): core.Express;
+export declare function e(): core.Express;
 
-declare namespace e {
+export declare namespace e {
   /**
    * This is a built-in middleware function in Express. It parses incoming requests with JSON payloads and is based on body-parser.
    * @since 4.16.0
@@ -123,5 +123,3 @@ declare namespace e {
   interface Router extends core.Router {}
   interface Send extends core.Send {}
 }
-
-export = e;
